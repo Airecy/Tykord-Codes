4792b32454a27c45e258a310e08f35d7761ca3cf
Brian W. Cox <brianwc@miltown.com>
2022-08-26 10:04:49 -0400

    Create Pasword-Unexpire.ps1
    created

---
+++ Pasword-Unexpire.ps1
@@ -0,0 +1,16 @@
+﻿Param (
+	[Parameter(Mandatory=$true)][string]$username
+)
+
+$user = Get-ADUser -Identity $username -Properties pwdlastset
+
+If ($user) {
+	$user.pwdlastset = 0
+	Set-ADUser -Instance $user
+	$user.pwdlastset = -1
+	Set-ADUser -Instance $user
+
+	Write-Host "Done"
+}
+
+Pause
